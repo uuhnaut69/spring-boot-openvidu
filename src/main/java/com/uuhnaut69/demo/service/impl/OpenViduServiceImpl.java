985e94a0bd86475e47725592d11b7c0038c57a7c
uuhnaut69 <uuhnaut96@gmail.com>
2020-11-06 17:57:21 +0700

    Update remove token

--- src/main/java/com/uuhnaut69/demo/service/impl/OpenViduServiceImpl.java
+++ src/main/java/com/uuhnaut69/demo/service/impl/OpenViduServiceImpl.java
@@ -1,7 +1,7 @@
 package com.uuhnaut69.demo.service.impl;
 
-import com.google.common.base.Strings;
 import com.uuhnaut69.demo.model.Conversation;
+import com.uuhnaut69.demo.rest.exception.BadRequestException;
 import com.uuhnaut69.demo.rest.exception.InternalServerErrorException;
 import com.uuhnaut69.demo.rest.payload.response.CallEvent;
 import com.uuhnaut69.demo.service.ConversationService;
@@ -45,8 +45,6 @@ public class OpenViduServiceImpl implements OpenViduService {
     if (!currentUsernameLogin.isPresent()) {
       throw new InternalServerErrorException("Can't get token from server at this time");
     }
-    String token = "";
-
     TokenOptions tokenOptions =
         new TokenOptions.Builder()
             .data(currentUsernameLogin.get())
@@ -59,38 +57,49 @@ public class OpenViduServiceImpl implements OpenViduService {
 
     if (Objects.nonNull(mapSessions.get(conversationId))) {
       log.info("Session of conversation {} already exists", conversationId);
+      // Generate a new token with the recently created tokenOptions
       try {
-        token = this.mapSessions.get(conversationId).generateToken(tokenOptions);
+        String token = this.mapSessions.get(conversationId).generateToken(tokenOptions);
+        // Update our collection storing the new token
         this.mapSessionNamesTokens.get(conversationId).put(currentUsernameLogin.get(), token);
-      } catch (OpenViduJavaClientException | OpenViduHttpException e) {
-        log.error(e.getMessage());
-      }
-    } else {
-      log.info("Generate new session for conversation {}", conversationId);
-      try {
-        Session session = this.openVidu.createSession();
-        token = session.generateToken(tokenOptions);
-        this.mapSessions.put(conversationId, session);
-        Map<String, String> tokenInfo = new ConcurrentHashMap<>();
-        tokenInfo.put(currentUsernameLogin.get(), token);
-        this.mapSessionNamesTokens.put(conversationId, tokenInfo);
-
-        // Push notification for members in conversation
-        Conversation conversation = conversationService.findById(UUID.fromString(conversationId));
-        if (!conversation.getMembers().isEmpty()) {
-          conversation.getMembers().stream()
-              .filter(user -> !user.getUsername().equalsIgnoreCase(currentUsernameLogin.get()))
-              .forEach(
-                  user ->
-                      this.messagingTemplate.convertAndSend(
-                          "/topic/" + user.getUsername() + ".call-event",
-                          new CallEvent(conversationId, conversation.getTitle())));
+        return token;
+      } catch (OpenViduJavaClientException e1) {
+        throw new BadRequestException(e1.getMessage());
+      } catch (OpenViduHttpException e2) {
+        log.error(e2.getMessage());
+        if (404 == e2.getStatus()) {
+          // Invalid sessionId (user left unexpectedly). Session object is not valid
+          // anymore. Clean collections and continue as new session
+          this.mapSessions.remove(conversationId);
+          this.mapSessionNamesTokens.remove(conversationId);
         }
-      } catch (OpenViduJavaClientException | OpenViduHttpException e) {
-        log.error(e.getMessage());
       }
     }
-    return token;
+
+    // New session
+    try {
+      Session session = this.openVidu.createSession();
+      String token = session.generateToken(tokenOptions);
+      this.mapSessions.put(conversationId, session);
+      Map<String, String> tokenInfo = new ConcurrentHashMap<>();
+      tokenInfo.put(currentUsernameLogin.get(), token);
+      this.mapSessionNamesTokens.put(conversationId, tokenInfo);
+
+      // Push notification for members in conversation
+      Conversation conversation = conversationService.findById(UUID.fromString(conversationId));
+      if (!conversation.getMembers().isEmpty()) {
+        conversation.getMembers().stream()
+            .filter(user -> !user.getUsername().equalsIgnoreCase(currentUsernameLogin.get()))
+            .forEach(
+                user ->
+                    this.messagingTemplate.convertAndSend(
+                        "/topic/" + user.getUsername() + ".call-event",
+                        new CallEvent(conversationId, conversation.getTitle())));
+      }
+      return token;
+    } catch (OpenViduJavaClientException | OpenViduHttpException e) {
+      throw new BadRequestException(e.getMessage());
+    }
   }
 
   @Override
@@ -98,13 +107,15 @@ public class OpenViduServiceImpl implements OpenViduService {
     Optional<String> currentUsernameLogin = getCurrentUserLogin();
     if (currentUsernameLogin.isPresent()
         && Objects.nonNull(this.mapSessions.get(conversationId))
-        && Objects.nonNull(this.mapSessionNamesTokens.get(conversationId))
-        && !Strings.isNullOrEmpty(
-            this.mapSessionNamesTokens.get(conversationId).remove(currentUsernameLogin.get()))
-        && this.mapSessionNamesTokens.get(conversationId).isEmpty()) {
-
-      // If session has no connection -> Remove it
-      this.mapSessions.remove(conversationId);
+        && Objects.nonNull(this.mapSessionNamesTokens.get(conversationId))) {
+      if (Objects.nonNull(
+          this.mapSessionNamesTokens.get(conversationId).remove(currentUsernameLogin.get()))) {
+        if (this.mapSessionNamesTokens.get(conversationId).isEmpty()) {
+          this.mapSessions.remove(conversationId);
+        }
+      } else {
+        throw new BadRequestException("Request failed");
+      }
     }
   }
 }
